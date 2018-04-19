4ece482c1cab2a52c2ae76b6cfcf57da147dd592
Arsh Chauhan <achauhan@alaska.edu>
2018-04-18 22:10:26 -0800

    Citizens can now make bids. Can pull all their bids but need to actually display it on the page and not just the console

--- server.py
+++ server.py
@@ -1,4 +1,8 @@
-from flask import Flask, render_template, request
+# Arsh Chauhan
+# Last Edited: 04/18/2018
+# Server for rootr. System to distribute root vegetables to the citizens of glorious Izkhstan
+
+from flask import Flask, render_template, request, jsonify
 from flask_pymongo import PyMongo
 
 app = Flask(__name__)
@@ -18,14 +22,25 @@ def get_izhk_balance(IZ_number):
 			return citizen['izhk']
 	except Exception as e:
 		new_citizen = mongo.db.citizens.insert({'IZ':int(IZ_number),'izhk':100})
-		print("EXCEPTION:" + str(e))
 		return get_izhk_balance(IZ_number)
-		# citizen = mongo.db.citizens.find_one({"IZ":IZ_number})
-		# return citizen['izhk']
 
 def update_izhk_balance(IZ_number,new_balance):
 	updated_citizen = mongo.db.citizens.find_one_and_update({"IZ":IZ_number},{"$set":{"izhk":new_balance}},upsert=True) 
 
+def add_bid_to_db(IZ_number,root,bid_amount):
+	mongo.db.bids.insert({"IZ":int(IZ_number),"root":root,"bid":bid_amount})
+
+@app.route('/all_bids_for_citizen/<int:IZ_number>',methods=['GET','POST'])
+def get_bids_for_citizen(IZ_number):
+	if IZ_number is None:
+		IZ_number = request.form['IZ_number']
+	bids = mongo.db.bids.find({'IZ':IZ_number})
+	bids_list=[]
+	for bid in bids:
+		bids_list.append({'root':bid['root'],'izhk':bid['bid']})
+	print (bids_list)
+	return jsonify(bids_list)
+	
 
 @app.route('/add_bid',methods=['POST'])
 def add_bid():
@@ -38,6 +53,7 @@ def add_bid():
 		new_balance = izhk_balance
 	else:
 		new_balance = izhk_balance - int(request.form['bid'])
+		add_bid_to_db(int(request.form['IZ_number']),request.form['roots'],int(request.form['bid']))
 		update_izhk_balance(int(request.form['IZ_number']),new_balance)
 		response += "<br> Bid successful. New Izhk balace is " + str(new_balance)
 	
--- static/home.js
+++ static/home.js
@@ -4,3 +4,20 @@ function check_bid_form()
 	var IZ_number = elements['IZ_number']; 
 	console.log(IZ_number);
 }
+
+function add_citizen_IZ(event)
+{
+	var elements = document.forms['get_all_bids_form'].elements;
+	var IZ_number = elements['IZ_number'].value;
+	var req = new XMLHttpRequest();
+	console.log(document.get_all_bids_form.action + '/'+IZ_number);
+	req.open('GET', document.get_all_bids_form.action +'/'+IZ_number, true);
+	req.onreadystatechange = function () 
+	{
+  		if(req.readyState === 4) 
+  		{
+    		console.log(req.responseText);
+    	}
+	}
+	req.send();
+}
